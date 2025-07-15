8cfd0bbabd684c48e0f8527e766ec252fa45edb8
Aniket Limaye <131771690+A-ni-ke-t@users.noreply.github.com>
2025-07-16 01:45:39 +0530

    Drawer closes after clicking on link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,7 +38,12 @@ const Navbar = () => {
             <ul>
               {navItems.map((item, index) => (
                 <li key={index} className="py-4">
-                  <a href={item.href}>{item.label}</a>
+                  <a
+                    href={item.href}
+                    onClick={() => setMobileDrawerOpen(false)}
+                  >
+                    {item.label}
+                  </a>
                 </li>
               ))}
             </ul>
